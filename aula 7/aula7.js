2ecc17578163877ed7324ecf2055011655e9ab55
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-02-25 08:33:35 -0300

    bind

--- aula 7/aula7.js
+++ aula 7/aula7.js
@@ -1,1 +1,16 @@
 console.log(this)
+
+const somar = function(n1, n2) {return n1 + n2}
+// O método bind cria uma nova função que, quando chamada, tem sua palavra-chave this definida com o valor fornecido. no caso 1 enquanto o outro valor não foi defenido
+const somar1 = somar.bind(null, 1)
+
+console.log(somar(4, 5))
+// realizara a somar 1 + 4, 0 5 será ignorado. O 4 é o n1, e o 1 n2
+console.log(somar1(4, 5))
+
+function func1(param1, param2) {
+    console.log(param1, param2)
+}
+
+const func2 = func1.bind(null, 'param1Fixo')
+func2('param2')
